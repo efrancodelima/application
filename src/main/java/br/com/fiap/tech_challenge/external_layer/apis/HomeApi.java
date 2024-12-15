dde239db7dbf60bd6d7cbeadfe79e1b9ab1f58df
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2024-12-15 17:57:34 -0300

    Ajustes em HomeApi.java

--- src/main/java/br/com/fiap/tech_challenge/external_layer/apis/HomeApi.java
+++ src/main/java/br/com/fiap/tech_challenge/external_layer/apis/HomeApi.java
@@ -1,15 +1,22 @@
 package br.com.fiap.tech_challenge.external_layer.apis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/")
 public class HomeApi {
 
+    @Autowired
+    private HttpServletRequest request;
+
     @GetMapping
     public String redirectToSwagger() {
-        return "redirect:/swagger-ui/index.html";
+        String currentUrl = request.getRequestURL().toString();
+        return "redirect:" + currentUrl + "swagger-ui/index.html";
     }
 }
