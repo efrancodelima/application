79fab3d0a27fadfbb0d3da8124c0c0cd79037040
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-01-15 20:37:18 -0300

    Atualização para aversão 2.1.0

--- src/main/java/br/com/fiap/techchallenge/TechChallengeApplication.java
+++ src/main/java/br/com/fiap/techchallenge/TechChallengeApplication.java
@@ -4,15 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
+/**
+ * Classe responsável por iniciar a aplicação.
+ */
 @SpringBootApplication
 public class TechChallengeApplication {
 
-	public static void main(String[] args) {
-		var application = new SpringApplication(TechChallengeApplication.class);
-		application.setApplicationStartup(new BufferingApplicationStartup(1024));
-		var context = application.run(args);
+  /**
+   * Método responsável por iniciar a aplicação.
+   */
+  public static void main(String[] args) {
+    var application = new SpringApplication(TechChallengeApplication.class);
+    application.setApplicationStartup(new BufferingApplicationStartup(1024));
+    var context = application.run(args);
 
-		PopularBD.popular(context);
-	}
+    PopularBd.popular(context);
+  }
 
 }
