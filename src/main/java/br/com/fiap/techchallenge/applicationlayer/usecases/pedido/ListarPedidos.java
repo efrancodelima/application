59b725840ba2315385fc5175f02bf02021b344dc
F2598806 Ederson Franco de Lima <ederson.lima@bb.com.br>
2025-01-18 11:26:27 -0300

    Correção na classe ListarPedidos

--- src/main/java/br/com/fiap/techchallenge/applicationlayer/usecases/pedido/ListarPedidos.java
+++ src/main/java/br/com/fiap/techchallenge/applicationlayer/usecases/pedido/ListarPedidos.java
@@ -1,27 +1,34 @@
 package br.com.fiap.techchallenge.applicationlayer.usecases.pedido;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import br.com.fiap.techchallenge.applicationlayer.exceptions.messages.EnumNotFoundExceptions;
 import br.com.fiap.techchallenge.applicationlayer.interfaces.gateway.InPedidoGateway;
 import br.com.fiap.techchallenge.applicationlayer.services.Validar;
 import br.com.fiap.techchallenge.businesslayer.entities.pedido.Pedido;
 import br.com.fiap.techchallenge.businesslayer.entities.pedido.StatusPedidoEnum;
+import java.util.Comparator;
+import java.util.List;
 
+/**
+ * Listar pedidos.
+ */
 public final class ListarPedidos {
 
   private ListarPedidos() {}
 
+  /**
+   * Listar pedidos.
+   *
+   * @param gateway O gateway para conexão com o repositório.
+   * @return A lista de pedidos.
+   */
   public static List<Pedido> listar(InPedidoGateway gateway) throws Exception {
     List<Pedido> pedidos = gateway.buscarTodosOsPedidos();
     Validar.listNotEmpty(pedidos, EnumNotFoundExceptions.PEDIDO_LISTA_VAZIA);
-    return filtrarEOrdenarPedidos(pedidos);
+    return filtrarOrdenarPedidos(pedidos);
   }
 
   // Métodos privados
-  private static List<Pedido> filtrarEOrdenarPedidos(List<Pedido> pedidos) {
+  private static List<Pedido> filtrarOrdenarPedidos(List<Pedido> pedidos) {
     return pedidos.stream()
         .filter(p -> p.getStatusPedido().getStatus() != StatusPedidoEnum.AGUARDANDO_CHECKOUT &&
             p.getStatusPedido().getStatus() != StatusPedidoEnum.FINALIZADO)
